4cb0ec0c07b61afa19aec6a29e509d99dcb1fca7
ihyeonu2312 <dlgusdn2312@gmail.com>
2025-01-21 16:34:44 +0900

    Todo

---
+++ DgTodo/app/src/main/java/site/unoeyhi/dgtodo/data/Todo.kt
@@ -0,0 +1,12 @@
+package site.unoeyhi.dgtodo.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Todo (
+    @PrimaryKey(autoGenerate = true)
+    val num: Int = 0,
+    val title: String,
+    val completed: Boolean
+)
---
+++ DgTodo/app/src/main/java/site/unoeyhi/dgtodo/ui/MainActivity.kt
@@ -0,0 +1,4 @@
+package site.unoeyhi.dgtodo.ui
+
+class MainActivity {
+}
